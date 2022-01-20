42fb539b2c540259227cc05bf39bc633fa2cf04e
L2ncE <llance_24@foxmail.com>
2022-01-20 11:01:42 +0800

    feat(api):路由更新

--- api/router.go
+++ api/router.go
@@ -19,6 +19,29 @@ func InitEngine() {
 		userGroup.POST("/password", changePassword) //修改密码
 	}
 
+	topicGroup := engine.Group("/topic")
+	{
+		{
+			topicGroup.Use(JWTAuth)                         //需要token
+			topicGroup.POST("/", addTopic)                  //发布新话题
+			topicGroup.DELETE("/:topic_id", deleteTopic)    //删除话题
+			topicGroup.POST("/:topic_id/likes", topicLikes) //给话题点赞
+		}
+		topicGroup.GET("/", briefTopics)          //查看全部话题概略
+		topicGroup.GET("/:topic_id", topicDetail) //查看一条话题详细信息和其下属评论
+	}
+
+	commentGroup := engine.Group("/comment")
+	{
+		commentGroup.POST("/anonymity", addCommentAnonymity) //匿名评论
+		{
+			commentGroup.Use(JWTAuth)                             //需要token
+			commentGroup.POST("/", addComment)                    //发送评论
+			commentGroup.DELETE("/:comment_id", deleteComment)    //删除评论
+			commentGroup.POST("/:comment_id/likes", commentLikes) //给评论点赞
+		}
+	}
+
 	err := engine.Run()
 	if err != nil {
 		return
