6e0a51eee9acfa576e0bc20c33494663ef3c1db5
L2ncE <llance_24@foxmail.com>
2022-01-20 10:01:25 +0800

    feat(model):电影详细页

---
+++ model/movie.go
@@ -0,0 +1,27 @@
+package model
+
+type Movie struct {
+	Id           int
+	Name         string
+	Year         string
+	Director     string
+	Screenwriter string
+	Starring     string
+	Type         string
+	Country      string
+	Language     string
+	ReleaseDate  string
+	Length       string
+	Aka          string
+	IMDb         string
+	StarNum      int
+	Score        string
+	OneStar      string
+	TwoStar      string
+	ThreeStar    string
+	FourStar     string
+	FiveStar     string
+	HaveWatched  int
+	WantToWatch  int
+	Synopsis     string
+}
