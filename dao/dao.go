9cf9d2620b550c6ea869dae372a25fa110ce92af
L2ncE <llance_24@foxmail.com>
2022-01-20 09:56:08 +0800

    refactor(dao):更改数据库服务器

--- dao/dao.go
+++ dao/dao.go
@@ -8,7 +8,7 @@ import (
 var dB *sql.DB
 
 func InitDB() (err error) {
-	dsn := "lance:yxh030714@tcp(127.0.0.1:3306)/douban?charset=utf8&parseTime=True"
+	dsn := "lance:yxh030714@tcp(42.192.155.29:3306)/douban?charset=utf8&parseTime=True"
 	// 连接数据库
 	dB, err = sql.Open("mysql", dsn)
 	if err != nil {
