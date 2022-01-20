68a86028d997ddf7a804f35e33c8df749e7b6ab1
L2ncE <llance_24@foxmail.com>
2022-01-20 09:59:11 +0800

    feat(dao):电影详细页

---
+++ dao/movie.go
@@ -0,0 +1,20 @@
+package dao
+
+import "douban/model"
+
+// SelectMovieById 通过id来搜索电影
+func SelectMovieById(movieId int) (model.Movie, error) {
+	var movie model.Movie
+
+	row, err := dB.Query("SELECT id, name, year, director, screenwriter, starring, type, country, language, releasedate, length, aka, imdb, starnum, score, onestar, twostar, threestar, fourstar, fivestar, havewatched, wanttowatch, synopsis FROM movie WHERE id = ? ", movieId)
+	if err != nil {
+		return movie, row.Err()
+	}
+
+	err = row.Scan(&movie.Id, &movie.Name, &movie.Year, &movie.Director, &movie.Screenwriter, &movie.Starring, &movie.Type, &movie.Country, &movie.Language, &movie.ReleaseDate, &movie.Length, &movie.Aka, &movie.IMDb, &movie.StarNum, &movie.Score, &movie.OneStar, &movie.TwoStar, &movie.ThreeStar, &movie.FourStar, &movie.FiveStar, &movie.HaveWatched, &movie.WantToWatch, &movie.Synopsis)
+	if err != nil {
+		return movie, err
+	}
+
+	return movie, nil
+}
