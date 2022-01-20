7272e325e755fca1512d84bf98ae6167a79d58ea
L2ncE <llance_24@foxmail.com>
2022-01-20 16:22:37 +0800

    refactor(dao):更改电影详细页

--- dao/movie.go
+++ dao/movie.go
@@ -6,12 +6,12 @@ import "douban/model"
 func SelectMovieById(movieId int) (model.Movie, error) {
 	var movie model.Movie
 
-	row := dB.QueryRow("SELECT id, name, year, director, screenwriter, starring, type, country, language, releasedate, length, aka, imdb, starnum, score, onestar, twostar, threestar, fourstar, fivestar, havewatched, wanttowatch, synopsis FROM movie WHERE id = ? ", movieId)
+	row := dB.QueryRow("SELECT id, name, year, director, screenwriter, starring, type, country, language, length, imdb, starnum, score, onestar, twostar, threestar, fourstar, fivestar, havewatched, wanttowatch, synopsis, URL FROM movie WHERE id = ? ", movieId)
 	if row.Err() != nil {
 		return movie, row.Err()
 	}
 
-	err := row.Scan(&movie.Id, &movie.Name, &movie.Year, &movie.Director, &movie.Screenwriter, &movie.Starring, &movie.Type, &movie.Country, &movie.Language, &movie.ReleaseDate, &movie.Length, &movie.Aka, &movie.IMDb, &movie.StarNum, &movie.Score, &movie.OneStar, &movie.TwoStar, &movie.ThreeStar, &movie.FourStar, &movie.FiveStar, &movie.HaveWatched, &movie.WantToWatch, &movie.Synopsis)
+	err := row.Scan(&movie.Id, &movie.Name, &movie.Year, &movie.Director, &movie.Screenwriter, &movie.Starring, &movie.Type, &movie.Country, &movie.Language, &movie.Length, &movie.IMDb, &movie.StarNum, &movie.Score, &movie.OneStar, &movie.TwoStar, &movie.ThreeStar, &movie.FourStar, &movie.FiveStar, &movie.HaveWatched, &movie.WantToWatch, &movie.Synopsis, &movie.URL)
 	if err != nil {
 		return movie, err
 	}
