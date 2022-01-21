50fbd85daf7ab9051bf3283ef5080f5a63ce1810
L2ncE <llance_24@foxmail.com>
2022-01-21 10:52:26 +0800

    feat(dao):新增查看所有电影功能

--- dao/movie.go
+++ dao/movie.go
@@ -6,15 +6,38 @@ import "douban/model"
 func SelectMovieById(movieId int) (model.Movie, error) {
 	var movie model.Movie
 
-	row := dB.QueryRow("SELECT id, name, year, director, screenwriter, starring, type, country, language, length, imdb, starnum, score, onestar, twostar, threestar, fourstar, fivestar, havewatched, wanttowatch, synopsis, URL FROM movie WHERE id = ? ", movieId)
+	row := dB.QueryRow("SELECT id, name, year, director, screenwriter, starring, type, country, language, length, imdb, starnum, score, onestar, twostar, threestar, fourstar, fivestar, havewatched, wanttowatch, synopsis, URL, peopleURL FROM movie WHERE id = ? ", movieId)
 	if row.Err() != nil {
 		return movie, row.Err()
 	}
 
-	err := row.Scan(&movie.Id, &movie.Name, &movie.Year, &movie.Director, &movie.Screenwriter, &movie.Starring, &movie.Type, &movie.Country, &movie.Language, &movie.Length, &movie.IMDb, &movie.StarNum, &movie.Score, &movie.OneStar, &movie.TwoStar, &movie.ThreeStar, &movie.FourStar, &movie.FiveStar, &movie.HaveWatched, &movie.WantToWatch, &movie.Synopsis, &movie.URL)
+	err := row.Scan(&movie.Id, &movie.Name, &movie.Year, &movie.Director, &movie.Screenwriter, &movie.Starring, &movie.Type, &movie.Country, &movie.Language, &movie.Length, &movie.IMDb, &movie.StarNum, &movie.Score, &movie.OneStar, &movie.TwoStar, &movie.ThreeStar, &movie.FourStar, &movie.FiveStar, &movie.HaveWatched, &movie.WantToWatch, &movie.Synopsis, &movie.URL, &movie.PeopleURL)
 	if err != nil {
 		return movie, err
 	}
 
 	return movie, nil
+}
+
+// SelectMovie 查找电影
+func SelectMovie() ([]model.Movie, error) {
+	var movies []model.Movie
+	rows, err := dB.Query("SELECT id, name, year, director, screenwriter, starring, type, country, language, length, imdb, starnum, score, onestar, twostar, threestar, fourstar, fivestar, havewatched, wanttowatch, synopsis, URL, peopleURL FROM movie")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		var movie model.Movie
+
+		err = rows.Scan(&movie.Id, &movie.Name, &movie.Year, &movie.Director, &movie.Screenwriter, &movie.Starring, &movie.Type, &movie.Country, &movie.Language, &movie.Length, &movie.IMDb, &movie.StarNum, &movie.Score, &movie.OneStar, &movie.TwoStar, &movie.ThreeStar, &movie.FourStar, &movie.FiveStar, &movie.HaveWatched, &movie.WantToWatch, &movie.Synopsis, &movie.URL, &movie.PeopleURL)
+		if err != nil {
+			return nil, err
+		}
+
+		movies = append(movies, movie)
+	}
+
+	return movies, nil
 }
