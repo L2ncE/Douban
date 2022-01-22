c0289243e3f3a7c74f1eb31e5f2fc4a4f06c3e3b
L2ncE <llance_24@foxmail.com>
2022-01-22 10:35:02 +0800

    feat(model):更新短评功能

--- model/shortComment.go
+++ model/shortComment.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type ShortComment struct {
-	Id          int
-	MovieId     int
-	Name        string
-	Status      string
-	StarNum     int
-	CommentTime time.Time
-	Likes       int
-	Context     string
+	Id          int       `json:"id"`
+	MovieId     int       `json:"MovieId"`
+	Name        string    `json:"Name"`
+	Status      string    `json:"Status"`
+	StarNum     int       `json:"StarNum"`
+	CommentTime time.Time `json:"CommentTime"`
+	Likes       int       `json:"Likes"`
+	Context     string    `json:"Context"`
 }
