239f72c0cebd22364bda0a360567e4464059f859
L2ncE <llance_24@foxmail.com>
2022-01-28 17:01:03 +0800

    feat(model):更新影人页

---
+++ model/celebrity.go
@@ -0,0 +1,12 @@
+package model
+
+type Celebrity struct {
+	Id       int
+	Name     string
+	Info     string
+	Synopsis string
+	URLInfo1 string
+	URLInfo2 string
+	URLInfo3 string
+	Award    string
+}
