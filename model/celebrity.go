e07f1203e1d0caf2fe3ddf9bf395af531d04934e
L2ncE <llance_24@foxmail.com>
2022-01-30 10:35:57 +0800

    refactor(dao):修改影人模型

--- model/celebrity.go
+++ model/celebrity.go
@@ -1,13 +1,17 @@
 package model
 
 type Celebrity struct {
-	Id       int
-	Name     string
-	Info     string
-	Synopsis string
-	URL      string
-	URLInfo1 string
-	URLInfo2 string
-	URLInfo3 string
-	Award    string
+	Id        int
+	Name      string
+	Info      string
+	Synopsis  string
+	URL       string
+	URLInfo1  string
+	URLInfo2  string
+	URLInfo3  string
+	Award     string
+	NameInfo1 string
+	NameInfo2 string
+	NameInfo3 string
+	YearInfo  string
 }
